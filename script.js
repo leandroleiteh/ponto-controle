c1b034c700ae4d0cbcb07f32d0176e027573bb07
Leandro Leite <leandroluz201616@gmail.com>
2024-12-02 18:32:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -351,3 +351,16 @@ function resetPassword() {
         alert('Por favor, preencha todos os campos.');
     }
 }
+
+//
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I') || (event.ctrlKey && event.shiftKey && event.key === 'J') || (event.ctrlKey && event.key === 'U')) {
+        event.preventDefault();
+    }
+});
+
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+})
+
+//
